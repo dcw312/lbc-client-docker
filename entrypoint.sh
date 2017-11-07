ea4d0bbf62246d8b4e218d4b879575dcaad9047d
dcw312 <david.craig.wilson@gmail.com>
2017-11-07 07:59:15 -0600

    defer status update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,3 @@
 touch FOUND.txt
 ./LBC --id $LBC_ID --secret $LBC_SECRET --cpus $LBC_CPU --loop $LBC_LOOP --address $LBC_BTC_ADDR
 ./send-it.sh
-./LBC --id $LBC_ID --secret $LBC_SECRET -q > QUERY.txt
-./send-query.sh
