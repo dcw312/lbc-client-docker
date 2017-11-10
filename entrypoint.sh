eb0bb7b0eee4aab74ab66f92ad640e3d0c79612b
David Wilson <david.craig.wilson@gmail.com>
2017-11-10 08:13:40 -0600

    Fixes #3 including address everytime causes spam on LBC server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-touch FOUND.txt
-./LBC --id $LBC_ID --secret $LBC_SECRET --cpus $LBC_CPU --loop $LBC_LOOP --address $LBC_BTC_ADDR
+./LBC --id $LBC_ID --secret $LBC_SECRET --cpus $LBC_CPU --loop $LBC_LOOP
 ./send-it.sh
---
+++ register.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./LBC --id $LBC_ID --secret nopassword:$LBC_SECRET --cpus $LBC_CPU --loop $LBC_LOOP --address $LBC_BTC_ADDR
